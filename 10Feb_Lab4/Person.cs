3bc127e4be1f893e8b7685f9f3f5c250d8ad403f
Adil Heybetov <heybetov1998@gmail.com>
2023-02-14 23:49:41 +0400

    initial

---
+++ 10Feb_Lab4/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace _10Feb_Lab4
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person() { }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
---
+++ 10Feb_Lab4/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _10Feb_Lab4
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region XML Serialize and Deserialize
+            //var people = new List<Person>
+            //{
+            //    new Person("Adil","Heybetov",24),
+            //    new Person("Arif","Babayev",17),
+            //    new Person("Huseyn","Aliyev",23),
+            //    new Person("Huseyn","Quliyev",17),
+            //};
+
+            //var serializer = new XmlSerializer(typeof(List<Person>));
+
+            //using (var writer = new StreamWriter("people.xml"))
+            //{
+            //    serializer.Serialize(writer, people);
+            //}
+            //List<Person> deserializedPeople = (List<Person>)serializer.Deserialize(File.OpenRead("people.xml"));
+
+            //foreach (var person in deserializedPeople)
+            //{
+            //    Console.WriteLine($"{"Firstname:",-10}{person.FirstName}");
+            //    Console.WriteLine($"{"Lastname:",-10}{person.LastName}");
+            //    Console.WriteLine($"{"Age:",-10}{person.Age}\n");
+            //}
+            #endregion
+
+            #region Writing Fetched JSON to file
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            await WriteFetchedJsonToFileAsync(url);
+            #endregion
+        }
+
+        static async Task WriteFetchedJsonToFileAsync(string url)
+        {
+            using (HttpClient client = new())
+            {
+                string json = await client.GetStringAsync(url);
+                
+                await File.Create("test.json").DisposeAsync();
+                
+                await File.WriteAllTextAsync("test.json", json);
+            }
+        }
+    }
+}
